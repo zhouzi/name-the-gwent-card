f814147840e03f12d199d1854b40e02c67a1a0bc
Gabin Aureche <hello@gabinaureche.com>
2020-06-08 21:28:58 +0200

    Typo

--- src/app/i18n/fr/messages.ts
+++ src/app/i18n/fr/messages.ts
@@ -1,11 +1,11 @@
 export const messages = {
   playSolo: "Jouer seul",
   gameInstructions:
-    "Dans ce mini jeu, ton objectif est de nommber une carte aléatoire Gwent depuis son illustration. Les cartes utilisées dans ce jeu viennent du <meta>dernier rapport de méta</meta> de <leviathan>Team Leviathan</leviathan>. C'est un bon moyen pour s'améliorer tout en s'amusant !",
+    "Dans ce mini jeu, ton objectif est de nommer une carte aléatoire Gwent depuis son illustration. Les cartes utilisées dans ce jeu viennent du <meta>dernier rapport de méta</meta> de <leviathan>Team Leviathan</leviathan>. C'est un bon moyen pour s'améliorer tout en s'amusant !",
   difficultyLevel:
     "{difficultyLevel, select, easy {<b>Facile</b>: les illustrations ne sont pas modifiées, tu recevras des indices.} medium {<b>Moyen</b>: les illustrations sont légèrement modifiées, pas d'indice.} hard {<b>Difficile</b>: les illustrations sont fortement modifiées, pas d'indice.} extreme {<b>Extrême</b>: les illustrations sont extrêmement modifiées, pas d'indice.}}",
   play: "Jouer",
-  playWithTwitchChat: "Jouer avec votre chat Twitch",
+  playWithTwitchChat: "Jouer avec ton chat Twitch",
   inspiredBy: "Inspiré par <a>Faberstein</a>",
   aboutCode: "À propos/Code",
   disclaimer:
